4ecffeb6f5a690a6734b14f1bd5e880cd645ecf4
andres <andresalejandroninoaraujo@gmail.com>
2023-01-07 16:21:52 -0500

    holi

---
+++ 1/hola.js
@@ -0,0 +1,4 @@
+// Hola, este es mi primer archivo en js - este es un comentario de una linea
+// Bienvenido a 30 dias de javascript
+/* Los comentarios pueden hacer el código legible, 
+fácil de usar e informativo*/
