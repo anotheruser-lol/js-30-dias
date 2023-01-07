0b0b7d47c370da44481c6c6e5ced5958eca55fdb
andres <andresalejandroninoaraujo@gmail.com>
2023-01-07 17:11:56 -0500

    dia 1

---
+++ 1/variable.js
@@ -0,0 +1,31 @@
+// string
+let nombre = 'Andres'
+
+// boolean
+let estaCasado = false
+
+// undefined
+let weight
+
+// null
+let valorVacio = null
+
+console.log(nombre, estaCasado, weight, valorVacio)
+
+let sabor
+let color
+let cantidad
+let olor
+
+let peso = 45
+let largo = 15 
+let ancho = 17
+let tamaño = 'amplio'
+
+let name; let lastName; let civilStatus; let country; let age
+
+let miEdad = 'Tengo 25 años'
+let tuEdad = 'Tu tienes 30 años'
+
+console.log(miEdad)
+console.log(tuEdad)
